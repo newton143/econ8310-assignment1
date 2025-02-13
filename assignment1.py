24b2aad16c3db52b40cd968471097bca32cff9e8
New-10 <newton143@gmail.com>
2025-02-12 22:43:11 -0600

    Final
    Final

--- assignment1.py
+++ assignment1.py
@@ -2,22 +2,20 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 from pygam import LinearGAM, s
-import pickle
 
 # Load Training Data
 df_train = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv", parse_dates=['Timestamp'])
 df_train['Timestamp'] = pd.to_datetime(df_train['Timestamp'])
 df_train.set_index('Timestamp', inplace=True)
 
-# Ensure hourly frequency & drop missing values
+# Set Frequency as Hours in Training Data
 df_train = df_train.asfreq('h').dropna()
 
-# Extract exogenous features
+# Set exogenous features
 df_train['year'] = df_train.index.year
 df_train['month'] = df_train.index.month
 df_train['day'] = df_train.index.day
 df_train['hour'] = df_train.index.hour
-
 exog_vars = ['year', 'month', 'day', 'hour']
 X_train = df_train[exog_vars].astype(np.float64).values  
 y_train = df_train['trips'].astype(np.float64).values
@@ -25,10 +23,6 @@ y_train = df_train['trips'].astype(np.float64).values
 # Train GAM Model
 model = LinearGAM(s(0) + s(1) + s(2) + s(3))
 modelFit = model.fit(X_train, y_train)
-
-# Save the trained model
-with open("model.pkl", "wb") as f:
-    pickle.dump(model, f)
 
 # Generate Predictions for Training Data
 y_pred_train = model.predict(X_train)
@@ -38,25 +32,22 @@ df_test = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/m
 df_test['Timestamp'] = pd.to_datetime(df_test['Timestamp'])
 df_test.set_index('Timestamp', inplace=True)
 
-# Ensure hourly frequency & drop missing values
+# Set Frequency as Hours in Test Data
 df_test = df_test.asfreq('h').dropna()
 
-# Extract exogenous features
+# Set exogenous features
 df_test['year'] = df_test.index.year
 df_test['month'] = df_test.index.month
 df_test['day'] = df_test.index.day
 df_test['hour'] = df_test.index.hour
-
 X_test = df_test[exog_vars].astype(np.float64).values  
 
 # Generate Predictions for Test Data
 y_pred_test = model.predict(X_test)
 pred = y_pred_test
 
-# Convert predictions to integers
-df_test['predicted_trips'] = np.round(y_pred_test).astype(int)  
-
-# Save to CSV
+# # Save to CSV
+df_test['predicted_trips'] = np.round(y_pred_test).astype(int)
 df_test[['predicted_trips']].to_csv("predictions_gam.csv")
 
 # Plot Results
